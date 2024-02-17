39ffd0998efa9ff1e18d9848488e3a4e458fd44e
Andriy0 <andriy2152@gmail.com>
2024-02-18 01:30:19 +0200

    Move report deletion handling to reports list live component

--- lib/csp_dashboard_phx_web/live/violation_reports_list_component.ex
+++ lib/csp_dashboard_phx_web/live/violation_reports_list_component.ex
@@ -2,6 +2,20 @@ defmodule CspDashboardPhxWeb.ViolationReportsListComponent do
   use CspDashboardPhxWeb, :live_component
   import CspDashboardPhxWeb.Live.Helpers
 
+  alias CspDashboardPhx.ViolationReports
+
+  def handle_event("delete-report", %{"report_id" => id}, socket) do
+    violation_report = ViolationReports.get_violation_report!(id)
+
+    with {:ok, _violation_report} = ViolationReports.delete_violation_report(violation_report) do
+      violation_reports =
+        socket.assigns.violation_reports
+        |> Enum.reject(&(&1.id == id))
+
+      {:noreply, assign(socket, violation_reports: violation_reports)}
+    end
+  end
+
   defp open_show_report_modal(report_id) do
     %JS{}
     |> JS.push("open-show-report-modal", value: %{report_id: report_id})
--- lib/csp_dashboard_phx_web/live/violation_reports_live.ex
+++ lib/csp_dashboard_phx_web/live/violation_reports_live.ex
@@ -42,16 +42,4 @@ defmodule CspDashboardPhxWeb.ViolationReportsLive do
   def handle_event("reset-filter", _params, socket) do
     {:noreply, push_patch(socket, to: ~p"/violation_reports")}
   end
-
-  def handle_event("delete-report", %{"report_id" => id}, socket) do
-    violation_report = ViolationReports.get_violation_report!(id)
-
-    with {:ok, _violation_report} = ViolationReports.delete_violation_report(violation_report) do
-      violation_reports =
-        socket.assigns.violation_reports
-        |> Enum.reject(&(&1.id == id))
-
-      {:noreply, assign(socket, violation_reports: violation_reports)}
-    end
-  end
 end
